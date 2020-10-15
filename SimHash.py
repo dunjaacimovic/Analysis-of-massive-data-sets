1ae8f7f62a2ee2642781b5e62c87e81925cc2066
dunjaacimovic <dunjaacimovicc@gmail.com>
2020-10-15 13:56:48 +0200

    Add final version of Simhash.py

--- SimHash.py
+++ SimHash.py
@@ -14,73 +14,72 @@ def simhash(text):
     binary_result = ""
     for i in range(0, len(sh)):
         sh[i] = 1 if sh[i] >= 0 else 0
-        binary_result += str(sh[i])
 
-    hex_result = hex(int(binary_result, 2))
-    return str(hex_result[2:-1])
+    return sh
+    #     binary_result += str(sh[i])
+
+    # hex_result = hex(int(binary_result, 2))
+    # return str(hex_result[2:-1])
 
 
-def hammingDistance(x, y):
-
+def hammingDistance(x, y, k):
     ans = 0
-    x = int(x, 16)
-    y = int(y, 16)
-    for i in range(127,-1,-1):
-     b1= x>>i&1
-     b2 = y>>i&1
-     ans+= not(b1==b2)
+    # x = int(x, 16)
+    # y = int(y, 16)
+
+    # for i in range(127,-1,-1):
+    #  b1= x>>i&1
+    #  b2 = y>>i&1
+    #  ans+= not(b1==b2)
+
+    for i in range(128):
+        ans += not(x[i] == y[i])
+        if ans > k: return 0
+    return 1
+
+def main():
+
+    sh_lines = []
+    #     file = open(file, "r")
+    file = sys.stdin
+    n = int(file.readline())
+
+    sh_lines = [(simhash(file.readline())) for count in range(n)]
+        # # line = file.readline()
+        # sh_lines.append(simhash(file.readline()))
     
-#      if not(b1==b2):
-#         print(b1,b2,i, "diff")
-#      else:
-#         print(b1,b2,i)
-    return ans
+    q = int(file.readline())
+    for count in range(q):
+        result = 0
+        line = file.readline()
 
-# def main():
+        [i, k] =  line.split()
+    #         print i, k
+        i = int(i); k = int(k)
+        
+        s1 = sh_lines[i]
+        # s2 = sh_lines[0]
+        # print s1
+        # print s2
+        # print hammingDistance(s1, s2)
 
-sh_lines = []
-#     file = open(file, "r")
-file = sys.stdin
-n = file.readline()
-
-for count in range(0, int(n)): 
-    line = file.readline()
-    sh_lines.append(simhash(line))
-    
-#     s1 = simhash("fakultet elektrotehnike i racunarstva")
-#     s2 = simhash("fakultet elektronike i racunarstva")
-#     print s1
-#     print s2
-#     ham = hammingDistance(s1, s2)
-#     print ham
-#     print type(ham)
-#     print sh_lines
-
-
-q = int(file.readline())
-for count in range(1, q): 
-    result = 0
-    line = file.readline()
-#     print(line)
-
-    [i, k] =  line.split()
-#         print i, k
-    i = int(i)
-    k = int(k)
-    
-    s1 = sh_lines[i]
-    for index in range(0, q):
-        if index == i: continue
-        s2 = sh_lines[index]
-        ham_dist = hammingDistance(s1, s2)
-        if ham_dist <= k:
-#                 print ham_dist
-            result += 1
-    sys.stdout.write(str(result) + "\n")
-    sys.stdout.write("hello")
-    # file.close()
+        for index in range(0, q):
+            if index == i: continue
+            # s2 = sh_lines[index]
+#     #         print s1
+#     #         print s2
+            
+            ham_dist = hammingDistance(s1, sh_lines[index], k)
+            result += ham_dist
+#             # print ham_dist
+    #         if ham_dist <= k:
+    # #                 print ham_dist
+    #             result += 1
+    #     print(str(result))
+        print result
+# #         # file.close()
 
   
-# if __name__== "__main__":
-#     main()
-#     sys.stdout.write("hello")
+if __name__== "__main__":
+    main()
+  
